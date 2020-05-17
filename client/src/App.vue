322ca32354bb0fbc721e18aaadb2c589c15374b0
devcrocodile <crazynet190@outlook.com>
2020-05-17 00:52:44 -0400

    initial layout added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,52 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-peanut</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="subtitle-1">Kernel Shop Front</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app color="indigo" dark>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Kernel Shoppe</v-toolbar-title>
+        </v-app-bar>
+
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row>
+                    <router-view></router-view>
+                </v-row>
+            </v-container>
+        </v-content>
+        <v-footer color="indigo" app>
+            <span class="white--text">&copy; 2020</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+    name: 'App',
+    data: () => ({
+        drawer: false,
+    }),
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
